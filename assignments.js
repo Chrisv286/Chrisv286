23fafbcea406affd04241f8d2f57d0e90ada975d
Chrisv286 <93850167+Chrisv286@users.noreply.github.com>
2022-02-06 19:40:00 -0800

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,65 @@
+let country = 'Portugal';
+let continent = 'Europe';
+let population = 10;
+console.log(country);
+console.log(continent);
+console.log(population);
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+language = 'Portuguese';
+const country = 'Portugal';
+const continent = 'Europe';
+const isIsland = false;
+isIsland = true;
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population < 6);
+console.log(population < 33);
+const description1 =
+ country +
+ ' is in ' +
+ continent +
+ ', and its ' +
+ population +
+ ' million people speak ' +
+ language;
+ console.log(description1);
+ const description = '${country} is in ${continent}, and its ${population} million people speak ${language};
+ if (population > 33) {
+     console.log(' ${country}'s population is above average
+     ); } else {
+         console.log(
+             '${country}'s population is ${33 - population} million below average',
+         );
+        }
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> '617'
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
+const numNeighbors = prompt(
+    'How many neighbor country does your country have?,
+);
+
+
+const numNeighbors = Number(
+  prompt('How many neighbor countries does your country have?'),
+);
+
+if (numNeighbors === 1) {
+    console.log('Only 1 border!');
+{ else if (numNeighbors > 1) 
+  console.log('More than 1 border');{
+} else {
+  console.log('No bordersl');
+}
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,25 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+
+
+
+
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
